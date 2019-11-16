d5c4d0e4d3ab3c005061ed3c3419ee8d32879837
kingastanek <kin.stanek@gmail.com>
2019-11-16 18:46:41 +0100

    Add Singleton Pattern

---
+++ singletonPattern.js
@@ -0,0 +1,28 @@
+function Process(state) {
+  this.state = state
+}
+
+const Singleton = (function() {
+  function ProcessManager() {
+    this.numProcess = 0
+  }
+
+  let processManager;
+
+  function createProcessManager() {
+    processManager = new ProcessManager;
+    return processManager;
+  }
+
+  return {
+    getProcessManager: () => {
+      if (!processManager) processManager = createProcessManager();
+      return processManager
+    }
+  }
+})()
+
+const processManager = Singleton.getProcessManager();
+const processManager2 = Singleton.getProcessManager();
+
+console.log(processManager === processManager2);
