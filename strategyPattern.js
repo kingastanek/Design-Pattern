ec8603a7a09f64c923980cb189195f8f5b2abdcd
kingastanek <kin.stanek@gmail.com>
2019-11-17 14:21:34 +0100

    Add Strategy Pattern

---
+++ strategyPattern.js
@@ -0,0 +1,53 @@
+function Company1() {
+  this.calculate = (package) => {
+    //company1 calculations...
+    return 1.22;
+  }
+}
+
+function Company2() {
+  this.calculate = (package) => {
+    //company2 calculations...
+    return 2.56;
+  }
+}
+
+function Company3() {
+  this.calculate = (package) => {
+    //company3 calculations...
+    return 2.88;
+  }
+}
+
+const company1 = new Company1();
+const company2 = new Company2();
+const company3 = new Company3();
+
+const package = { weight: 0.5 };
+
+//want we can do
+company1.calculate(package);
+company2.calculate(package);
+company3.calculate(package);
+
+//instead, we are using Strategy Patter
+function Shipping() {
+  this.company = '',
+  this.setStrategy = (company) => {
+    this.company = company;
+  }
+  this.calculate = (package) => {
+    return this.company.calculate(package);
+  }
+}
+
+const shipping = new Shipping;
+
+shipping.setStrategy(company1);
+console.log(`Company1: ${shipping.calculate(package)}`);
+
+shipping.setStrategy(company2);
+console.log(`Company2: ${shipping.calculate(package)}`);
+
+shipping.setStrategy(company3);
+console.log(`Company3: ${shipping.calculate(package)}`);
