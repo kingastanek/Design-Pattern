dfd6f550dafd8ad8cabfedadc3f650c63ac0ed96
kingastanek <kin.stanek@gmail.com>
2019-11-17 14:55:32 +0100

    Add Iterator Pattern

---
+++ interatorPattern.js
@@ -0,0 +1,19 @@
+const array = ['hello', 5, false, 3.14];
+
+function Iterator(array) {
+  this.array = array,
+  this.index = array.length - 1;
+}
+
+Iterator.prototype = {
+  hasNext: function() {
+    return this.index >= 0;
+  },
+  next: function() {
+    return this.array[this.index--];
+  }
+}
+
+const iterator = new Iterator(array);
+
+while(iterator.hasNext()) console.log(iterator.next())
